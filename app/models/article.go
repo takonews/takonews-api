9ef9bf55ac49a7e64b1f2f04c01b9d0a2d1aa9ae
chikyukotei <chikyukotei1122@gmail.com>
2016-08-30 14:49:55 +0900

    update article model

--- app/models/article.go
+++ app/models/article.go
@@ -4,15 +4,16 @@ import "time"
 
 // Article is a model of news articles
 type Article struct {
-	ID          uint `gorm:"primary_key"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   *time.Time
+	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+	// Article belongs to NewsSite
 	NewsSite    NewsSite `gorm:"ForeignKey:NewsSiteID;AssociationForeignKey:ID"`
-	NewsSiteID  uint     // Article belongs to NewsSite
-	URL         string
-	Title       string
+	NewsSiteID  uint     `gorm:"not null"`
+	URL         string   `gorm:"not null"`
+	Title       string   `gorm:"not null"`
 	Description string
-	FullText    string `gorm:"type:text"`
-	PublishedAt time.Time
+	FullText    string    `gorm:"type:text"`
+	PublishedAt time.Time `gorm:"not null"`
 }
